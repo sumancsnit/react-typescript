993be7891c9aee533afd654aa15d0b6d4726e956
Suman Kumar <sumancsnit@gmail.com>
2020-07-17 17:47:59 +0530

    style added,

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import './TodoList.css';
+
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 const TodoList: React.FC<TodoListProps> = (props) => {
+  const { onDeleteTodo, items } = props;
   return (
     <ul>
-      {props.items.map((todo) => (
-        <li key={todo.id}> {todo.text} </li>
+      {items.map((todo) => (
+        <li key={todo.id}>
+          <span> {todo.text} </span>
+          <button onClick={() => onDeleteTodo(todo.id)}>Delete</button>
+        </li>
       ))}
     </ul>
   );
